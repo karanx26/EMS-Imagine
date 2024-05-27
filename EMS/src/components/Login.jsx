5873414dba480fc51063b1fc802910f0d37b1bd8
karanx26 <Karan.tce21@sot.pdpu.ac.in>
2024-05-27 16:53:37 +0530

    form

--- EMS/src/components/Login.jsx
+++ EMS/src/components/Login.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
+import './style.css'
 
 const Login = () => {
   return (
-    <div>
-        <div>
+    <div className='d-flex justify-content-center align-items-center vh-100 loginPage'>
+        <div className='p-3 rounded w-25 border loginForm'>
             <h2>Login Page</h2>
             <form>
                 <div>
-
+                    <label htmlFor="email"><strong>Email:</strong></label>
+                    <input type="email" autoComplete='off' placeholder="Enter Email" name="email" 
+                    className='form-control rounded-0' />
                 </div>
+                <div>
+                    <label htmlFor="password"><strong>Password:</strong></label>
+                    <input type="password" placeholder="Enter Password" name="password" 
+                    className='form-control rounded-0' />
+                </div>
+                <button className='btn btn-success w-100 rounded-0'>Submit</button>
+                
             </form>
         </div>
 
