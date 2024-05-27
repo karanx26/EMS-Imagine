03bb1b1f558ba4d646eb0c36e57eff2049e90850
karanx26 <Karan.tce21@sot.pdpu.ac.in>
2024-05-27 16:03:02 +0530

    new files

---
+++ EMS/src/components/Login.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+        <div>
+            <h2>Login Page</h2>
+            <form>
+                <div>
+                    
+                </div>
+            </form>
+        </div>
+
+      
+    </div>
+  )
+}
+
+export default Login
