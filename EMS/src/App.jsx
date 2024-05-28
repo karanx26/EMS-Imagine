d313b521c7bf95ea3e4cbede093c83743326a9c7
karanx26 <Karan.tce21@sot.pdpu.ac.in>
2024-05-28 15:44:21 +0530

    homepage

--- EMS/src/App.jsx
+++ EMS/src/App.jsx
@@ -1,13 +1,18 @@
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Login from './components/Login'
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import HomePage from "./components/HomePage";
 
 function App() {
   return (
-    <>
-    <Login />
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        {/* <Route path="/Login" element={loggedIn? <Dashboard/>:<Login />}></Route> */}
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
