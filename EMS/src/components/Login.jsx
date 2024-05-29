4248b8cc71557fd384f36386fba4041438067f0f
karanx26 <Karan.tce21@sot.pdpu.ac.in>
2024-05-29 17:26:16 +0530

    font

--- EMS/src/components/Login.jsx
+++ EMS/src/components/Login.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-import '../styles/Login.css';
+import "../styles/Login.css";
 
 const Login = () => {
   return (
     <>
-    <div id="header">
+      <div id="header">
         <nav>
           <div id="logo-img">
             <img src="../Images/mainlogo.png" alt="logo" />
@@ -17,18 +17,30 @@ const Login = () => {
           </ul>
         </nav>
       </div>
-    <div className="d-flex justify-content-center align-items-center vh-100 loginPage">
+      <div className="d-flex justify-content-center align-items-center vh-100 loginPage">
         <div className="p-3 rounded w-25 border loginForm">
-        <h2 className="center-align">LogIn As</h2>
-            <div className="d-flex justify-content-between mt-5 mb-2">
-                <button type="button" className="btn btn-primary"><a className="buttonlink" href="loginforma">Admin</a></button>
-                <button type="button" className="btn btn-danger"><a className="buttonlink" href="loginforme">Employee</a></button>
-                <button type="button" className="btn btn-success"><a className="buttonlink" href="loginformc">Client</a></button>
-            </div>
-        </div>      
-    </div>
+          <h2 className="center-align">LogIn As</h2>
+          <div className="d-flex justify-content-between mt-5 mb-2">
+            <button type="button" className="btn btn-primary">
+              <a className="buttonlink" href="loginforma">
+                Admin
+              </a>
+            </button>
+            <button type="button" className="btn btn-danger">
+              <a className="buttonlink" href="loginforme">
+                Employee
+              </a>
+            </button>
+            <button type="button" className="btn btn-success">
+              <a className="buttonlink" href="loginformc">
+                Client
+              </a>
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   );
-}
+};
 
 export default Login;
--- EMS/src/components/LoginformE.jsx
+++ EMS/src/components/LoginformE.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import '../styles/Login.css';
+import React from "react";
+import "../styles/Login.css";
 
 function LoginformE() {
-return (
+  return (
     <>
-    <div id="header">
+      <div id="header">
         <nav>
           <div id="logo-img">
             <img src="../Images/mainlogo.png" alt="logo" />
@@ -16,28 +16,45 @@ return (
           </ul>
         </nav>
       </div>
-    <div className='d-flex justify-content-center align-items-center vh-100 loginPage'>
-        <div className='p-3 rounded w-25 border loginForm'>
-        <form>
-        
-        <h2 className="center-align">Employee</h2>
-  <div className='mb-3'>
-    <label htmlFor="uid"><strong>Unique Id</strong></label><br />
-    <input type="uid" autoComplete='off' placeholder="Enter UID" name="uid" className='form-control' id="uid" />
-  </div>
-  <div className='mb-3'>
-    <label htmlFor="password"><strong>Password</strong></label><br />
-    <input type="password" placeholder="Enter Password" name="password" className='form-control' id="password" />
-  </div>
-  <button className='btn btn-danger w-100 rounded-0 mb-2'>Log In</button>
-</form>
-
+      <div className="d-flex justify-content-center align-items-center vh-100 loginPage">
+        <div className="p-3 rounded w-25 border loginForm">
+          <form>
+            <h2 className="center-align">Employee</h2>
+            <div className="mb-3">
+              <label htmlFor="uid">
+                <strong>Unique Id</strong>
+              </label>
+              <br />
+              <input
+                type="uid"
+                autoComplete="off"
+                placeholder="Enter UID"
+                name="uid"
+                className="form-control"
+                id="uid"
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="password">
+                <strong>Password</strong>
+              </label>
+              <br />
+              <input
+                type="password"
+                placeholder="Enter Password"
+                name="password"
+                className="form-control"
+                id="password"
+              />
+            </div>
+            <button className="btn btn-danger w-100 rounded-0 mb-2">
+              Log In
+            </button>
+          </form>
         </div>
-
-      
-    </div>
+      </div>
     </>
-  )
-};
+  );
+}
 
 export default LoginformE;
