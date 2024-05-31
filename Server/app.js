f085596e5a4d6579537d0b0e327af242a1851865
rachit973 <rachit.vce21@sot.pdpu.ac.in>
2024-05-31 13:31:45 +0530

    mongodb

---
+++ Server/app.js
@@ -0,0 +1,7 @@
+const express = require("express")
+const collection = require("./index")
+const cors = require("cors")
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cors())
--- Server/index.js
+++ Server/index.js
@@ -1,8 +1,18 @@
 import express from "express";
 
+import mongoose from "mongoose";
 
+mongoose.connect("mongodb+srv://ipowertree:nP0d1pAEWVXpXYoK@imagine.onigxmo.mongodb.net/?retryWrites=true&w=majority&appName=imagine")
+.then(()=>{
+    console.log("mongodb connected");
+})
+.catch(()=>{
+    console.log("failed");
+})
 const app = express()
 
 app.listen(3000, () => {
     console.log("Server is running")
 })
+
+
