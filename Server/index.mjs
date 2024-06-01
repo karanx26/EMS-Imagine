3e76af3d7df55dbda704947005657ca203a1fce1
karanx26 <Karan.tce21@sot.pdpu.ac.in>
2024-06-01 12:24:19 +0530

    adash

--- Server/index.mjs
+++ Server/index.mjs
@@ -1,53 +1,7 @@
-// import mongoose, { mongo } from "mongoose";
-// import express from "express";
-
-// const someAsyncOperation = () => {
-//     return new Promise((resolve, reject) => {
-//         // Simulate an async operation using setTimeout
-//         setTimeout(() => {
-//             resolve('Success');
-//         }, 1000);
-//     });
-// };
-
-
-// mongoose.connect("mongodb+srv://ipowertree:nP0d1pAEWVXpXYoK@imagine.onigxmo.mongodb.net/?retryWrites=true&w=majority&appName=imagine", { useNewUrlParser: true, useUnifiedTopology: true });
-// someAsyncOperation()
-// .then(()=>{
-//     console.log("mongodb connected");
-// })
-// .catch(()=>{
-//     console.log("failed");
-// })
-// const app = express()
-
-// app.listen(8001, () => {
-//     console.log("Server is running")
-// })
-
-// const newSchema = new mongoose.Schema({
-//     uid: {
-//         type: String,
-//         required: true
-//     },
-
-//         password: {
-//         type: String ,
-//         required: true
-//     }
-// })
-
-// const collection = mongoose.model("collections",newSchema)
-
-// export default collection;
-
-
 import mongoose from "mongoose";
-// import express from "express";
 
 const someAsyncOperation = () => {
     return new Promise((resolve, reject) => {
-        // Simulate an async operation using setTimeout
         setTimeout(() => {
             resolve('Success');
         }, 1000);
@@ -74,6 +28,6 @@ const newSchema = new mongoose.Schema({
     }
 });
 
-const collection = mongoose.model("collections", newSchema);
+const collection = mongoose.model("admins", newSchema);
 
 export default collection;
