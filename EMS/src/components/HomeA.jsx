b155f494571091b46246749e46cf8f4920235c9d
rachit973 <rachit.vce21@sot.pdpu.ac.in>
2024-06-03 17:31:08 +0530

    css

--- EMS/src/components/HomeA.jsx
+++ EMS/src/components/HomeA.jsx
@@ -1,15 +1,14 @@
 import React from "react";
-// import { useLocation,useNavigate } from 'react-router-dom'
 import { Link, Outlet, useNavigate } from "react-router-dom";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import axios from "axios";
-import LoginformA from "./LoginformA";
+import "../styles/HomeA.css"; // Import your custom CSS
 
 function HomeA() {
   const navigate = useNavigate();
   axios.defaults.withCredentials = true;
+
   const handleLogout = () => {
-    // axios.get('http://localhost:5173/loginforma/logout')
     axios.get("http://localhost:8001/logout").then((result) => {
       if (result.data.Status) {
         localStorage.removeItem("isLoggedIn");
@@ -19,17 +18,14 @@ function HomeA() {
     });
   };
 
-  // const location = useLocation()
-
   return (
     <div className="container-fluid">
-      {/* <h1> Hello{location.state.id} and Welcome to Dashboard</h1> */}
       <div className="row flex-nowrap">
-        <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
+        <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark sidebar">
           <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-yellow min-vh-100">
             <Link
               to="/HomeA"
-              className="d-flex align-items-center pb-3 mb-md-1 mt-md-3 me-md-auto text-yellow text-decoration"
+              className="d-flex align-items-center pb-3 mb-md-1 mt-md-3 me-md-auto text-yellow text-decoration-none"
             >
               <span className="fs-5 fw-bolder d-none d-sm-inline">
                 Imagine Powertree
@@ -45,7 +41,6 @@ function HomeA() {
                   to="/HomeA"
                   className="nav-link text-white px-0 align-middle"
                 >
-                  {" "}
                   <i className="fs-4 bi-speedometer2 ms-2"></i>
                   <span className="ms-2 d-none d-sm-inline">
                     Admin Dashboard
@@ -70,10 +65,8 @@ function HomeA() {
                   to="/HomeA/Category"
                   className="nav-link px-0 align-middle text-white"
                 >
-                  {" "}
                   <i className="fs-4 bi-columns ms-2"></i>
-                  <span className="ms-2 d-none d-sm-inline"></span>
-                  Category
+                  <span className="ms-2 d-none d-sm-inline">Category</span>
                 </Link>
               </li>
 
@@ -82,7 +75,6 @@ function HomeA() {
                   to="/HomeA"
                   className="nav-link px-0 align-middle text-white"
                 >
-                  {" "}
                   <i className="fs-4 bi-person ms-2"></i>
                   <span className="ms-2 d-none d-sm-inline">Profile</span>
                 </Link>
@@ -101,7 +93,7 @@ function HomeA() {
         </div>
         <div className="col p-0 m-0">
           <div className="p-2 d-flex justify-content-center shadow">
-            <h4> Employee Managament </h4>
+            <h4> Employee Management </h4>
           </div>
           <Outlet />
         </div>
