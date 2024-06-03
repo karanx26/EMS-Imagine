c7f5e315ca69f177d3dee53c5a6b2f7496ceba09
karanx26 <Karan.tce21@sot.pdpu.ac.in>
2024-06-03 19:56:20 +0530

    EC

--- EMS/src/components/HomeA.jsx
+++ EMS/src/components/HomeA.jsx
@@ -1,110 +1,3 @@
-// import React from "react";
-// import { Link, Outlet, useNavigate } from "react-router-dom";
-// import "bootstrap-icons/font/bootstrap-icons.css";
-// import axios from "axios";
-// import "../styles/HomeA.css"; // Import your custom CSS
-
-// function HomeA() {
-//   const navigate = useNavigate();
-//   axios.defaults.withCredentials = true;
-
-//   const handleLogout = () => {
-//     axios.get("http://localhost:8001/logout").then((result) => {
-//       if (result.data.Status) {
-//         localStorage.removeItem("isLoggedIn");
-//         window.localStorage.removeItem("isLoggedIn");
-//         window.location.href = "/login"; // Redirect to login page
-//       }
-//     });
-//   };
-
-//   return (
-//     <div className="container-fluid">
-//       <div className="row flex-nowrap">
-//         <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark sidebar">
-//           <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-yellow min-vh-100">
-//             <Link
-//               to="/HomeA"
-//               className="d-flex align-items-center pb-3 mb-md-1 mt-md-3 me-md-auto text-yellow text-decoration-none"
-//             >
-//               <span className="fs-5 fw-bolder d-none d-sm-inline">
-//                 Imagine Powertree
-//               </span>
-//             </Link>
-
-//             <ul
-//               className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start"
-//               id="menu"
-//             >
-//               <li className="w-100">
-//                 <Link
-//                   to="/HomeA"
-//                   className="nav-link text-white px-0 align-middle"
-//                 >
-//                   <i className="fs-4 bi-speedometer2 ms-2"></i>
-//                   <span className="ms-2 d-none d-sm-inline">
-//                     Admin Dashboard
-//                   </span>
-//                 </Link>
-//               </li>
-
-//               <li className="w-100">
-//                 <Link
-//                   to="/HomeA/employee"
-//                   className="nav-link px-0 align-middle text-white"
-//                 >
-//                   <i className="fs-4 bi-people ms-2"></i>
-//                   <span className="ms-2 d-none d-sm-inline">
-//                     Manage Employees
-//                   </span>
-//                 </Link>
-//               </li>
-
-//               <li className="w-100">
-//                 <Link
-//                   to="/HomeA/Category"
-//                   className="nav-link px-0 align-middle text-white"
-//                 >
-//                   <i className="fs-4 bi-columns ms-2"></i>
-//                   <span className="ms-2 d-none d-sm-inline">Category</span>
-//                 </Link>
-//               </li>
-
-//               <li className="w-100">
-//                 <Link
-//                   to="/HomeA"
-//                   className="nav-link px-0 align-middle text-white"
-//                 >
-//                   <i className="fs-4 bi-person ms-2"></i>
-//                   <span className="ms-2 d-none d-sm-inline">Profile</span>
-//                 </Link>
-//               </li>
-//               <li className="w-100" onClick={handleLogout}>
-//                 <Link
-//                   to="/loginformA"
-//                   className="nav-link px-0 align-middle text-white"
-//                 >
-//                   <i className="fs-4 bi-power ms-2"></i>
-//                   <span className="ms-2 d-none d-sm-inline"> Logout </span>
-//                 </Link>
-//               </li>
-//             </ul>
-//           </div>
-//         </div>
-//         <div className="col p-0 m-0">
-//           <div className="p-2 d-flex justify-content-center shadow">
-//             <img src="../Images/mainlogo.png" alt="Logo" className="logo" />
-//           </div>
-//           <Outlet />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default HomeA;
-
-
 import React, { useEffect, useState } from "react";
 import { Link, Outlet, useNavigate } from "react-router-dom";
 import "bootstrap-icons/font/bootstrap-icons.css";
@@ -138,10 +31,10 @@ function HomeA() {
     <div className="container-fluid">
       <div className="row flex-nowrap">
         <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark sidebar">
-          <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-yellow min-vh-100">
+          <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-orange min-vh-100">
             <Link
               to="/HomeA"
-              className="d-flex align-items-center pb-3 mb-md-1 mt-md-3 me-md-auto text-yellow text-decoration-none"
+              className="d-flex align-items-center pb-3 mb-md-1 mt-md-3 me-md-auto text-orange text-decoration-none"
             >
               <span className="fs-5 fw-bolder d-none d-sm-inline">
                 {userId}
@@ -155,12 +48,10 @@ function HomeA() {
               <li className="w-100">
                 <Link
                   to="/HomeA"
-                  className="nav-link text-white px-0 align-middle"
+                  className="nav-link px-0 align-middle text-white"
                 >
-                  <i className="fs-4 bi-speedometer2 ms-2"></i>
-                  <span className="ms-2 d-none d-sm-inline">
-                    Admin Dashboard
-                  </span>
+                  <i className="fs-4 bi-person ms-2"></i>
+                  <span className="ms-2 d-none d-sm-inline">Profile</span>
                 </Link>
               </li>
 
@@ -178,26 +69,30 @@ function HomeA() {
 
               <li className="w-100">
                 <Link
-                  to="/HomeA/Category"
+                  to="/HomeA/employee"
                   className="nav-link px-0 align-middle text-white"
                 >
-                  <i className="fs-4 bi-columns ms-2"></i>
-                  <span className="ms-2 d-none d-sm-inline">Category</span>
+                  <i className="fs-4 bi-people ms-2"></i>
+                  <span className="ms-2 d-none d-sm-inline">
+                    Manage Clients
+                  </span>
                 </Link>
               </li>
 
               <li className="w-100">
                 <Link
-                  to="/HomeA"
+                  to="/HomeA/Category"
                   className="nav-link px-0 align-middle text-white"
                 >
-                  <i className="fs-4 bi-person ms-2"></i>
-                  <span className="ms-2 d-none d-sm-inline">Profile</span>
+                  <i className="fs-4 bi-columns ms-2"></i>
+                  <span className="ms-2 d-none d-sm-inline">Manage Inventory</span>
                 </Link>
               </li>
+
+              
               <li className="w-100" onClick={handleLogout}>
                 <Link
-                  to="/loginformA"
+                  to="/login"
                   className="nav-link px-0 align-middle text-white"
                 >
                   <i className="fs-4 bi-power ms-2"></i>
--- EMS/src/components/HomeC.jsx
+++ EMS/src/components/HomeC.jsx
@@ -1,20 +1,96 @@
-import React from 'react'
-// import { useLocation,useNavigate } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { Link, Outlet, useNavigate } from "react-router-dom";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import axios from "axios";
+import "../styles/HomeA.css"; // Import your custom CSS
 
-function HomeC(){
+function HomeC() {
+  const navigate = useNavigate();
+  const [userId, setUserId] = useState("");
 
-    // const location = useLocation()
+  axios.defaults.withCredentials = true;
 
-    return (
-        <div>
-            {/* <h1> Hello{location.state.id} and Welcome to Dashboard</h1> */}
-            <h1>Client Dashboard</h1>
+  useEffect(() => {
+    const uid = localStorage.getItem("uid");
+    if (uid) {
+      setUserId(uid);
+    }
+  }, []);
 
+  const handleLogout = () => {
+    axios.get("http://localhost:8001/logout").then((result) => {
+      if (result.data.Status) {
+        localStorage.removeItem("isLoggedIn");
+        localStorage.removeItem("uid");
+        window.location.href = "/login"; // Redirect to login page
+      }
+    });
+  };
 
+  return (
+    <div className="container-fluid">
+      <div className="row flex-nowrap">
+        <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark sidebar">
+          <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-orange min-vh-100">
+            <Link
+              to="/HomeA"
+              className="d-flex align-items-center pb-3 mb-md-1 mt-md-3 me-md-auto text-orange text-decoration-none"
+            >
+              <span className="fs-5 fw-bolder d-none d-sm-inline">
+                {userId}
+              </span>
+            </Link>
 
+            <ul
+              className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start"
+              id="menu"
+            >
+              <li className="w-100">
+                <Link
+                  to="/HomeA"
+                  className="nav-link px-0 align-middle text-white"
+                >
+                  <i className="fs-4 bi-person ms-2"></i>
+                  <span className="ms-2 d-none d-sm-inline">Profile</span>
+                </Link>
+              </li>
 
+              
+
+              
+
+              <li className="w-100">
+                <Link
+                  to="/HomeA/Category"
+                  className="nav-link px-0 align-middle text-white"
+                >
+                  <i className="fs-4 bi-bell ms-2"></i>
+                  <span className="ms-2 d-none d-sm-inline">Check Updates</span>
+                </Link>
+              </li>
+
+              
+              <li className="w-100" onClick={handleLogout}>
+                <Link
+                  to="/login"
+                  className="nav-link px-0 align-middle text-white"
+                >
+                  <i className="fs-4 bi-power ms-2"></i>
+                  <span className="ms-2 d-none d-sm-inline"> Logout </span>
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
-    )
+        <div className="col p-0 m-0">
+          <div className="p-2 d-flex justify-content-center shadow">
+            <img src="../Images/mainlogo.png" alt="Logo" className="logo" />
+          </div>
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default HomeC;
