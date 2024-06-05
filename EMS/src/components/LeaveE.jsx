a74d102fd71e77920b16f7ffc1c1a28e8a50c4af
rachit973 <rachit.vce21@sot.pdpu.ac.in>
2024-06-05 11:27:32 +0530

    leave

---
+++ EMS/src/components/LeaveE.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from "react";
+import { Link, Outlet, useNavigate } from "react-router-dom";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import axios from "axios";
+
+function LeaveE() 
+{
+  localStorage.removeItem("isLoggedIn");
+  window.localStorage.removeItem("isLoggedIn");
+  return(
+    <>
+      <h1>Leave</h1>
+    </>
+  )
+}
+  
+
+
+export default LeaveE;
